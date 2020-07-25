2f0a185aa44f1939f72ffaeef30211566f96fd01
adamthe1 <adamgabet@gmail.com>
2020-07-25 23:24:04 +0300

    added todos for two functions in fourstepcheck.py

--- fourstepcheck.py
+++ fourstepcheck.py
@@ -305,14 +305,17 @@ def isstockgood(stockchk):
     # TODO need take dictionary of checks of stocks and decide how good is stock and if to add to list or file of
     #  good stocks TODO by which order. order: check the notes in class stock start
     bestbuy = []  # where there are trendlines that the last is close to bot. macd has cut is amazing then
-    # eater then
+    # adx, 2 kind adx the +di is very up is good and if trendline slope is up the adx is over 30 is very good
+    #
+    # eater then hammer
     pass
 
 
 def writetofile(stock):
 
     # TODO write to file stock ticker and info why its a good stock ex.
-    # NFLX trendlines: 9 (7 up) (2 ok) closebot: 0,1,3 closetop: 4 hammer/eater: (date) macdcut: (date). adx good/bad +DI cut.
+    # NFLX trendlines: 9 (7 up) (2 ok) closebot: 0,1,3 closetop: 4 hammer/eater: (date) macdcut: (date).
+    # adx good/bad +DI cut.
 
     pass
 
